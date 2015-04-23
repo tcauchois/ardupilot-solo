71db6e43bb34962652a94f371d62022fcb7213fd
Jonathan Challinger <mr.challinger@gmail.com>
2015-04-22 18:19:07 -0700

    AP_Motors: refactor thrust curve function and add air density ratio

--- libraries/AP_Motors/AP_Motors_Class.cpp
+++ libraries/AP_Motors/AP_Motors_Class.cpp
@@ -116,6 +116,7 @@ AP_Motors::AP_Motors(RC_Channel& rc_roll, RC_Channel& rc_pitch, RC_Channel& rc_t
     _batt_current_resting(0.0f),
     _batt_resistance(0.0f),
     _batt_timer(0),
+    _air_density_ratio(1.0f),
     _lift_max(1.0f),
     _throttle_limit(1.0f),
     _throttle_in(0.0f),
@@ -289,12 +290,28 @@ void AP_Motors::current_limit_max_throttle()
 // apply_thrust_curve_and_volt_scaling - returns throttle curve adjusted pwm value (i.e. 1000 ~ 2000)
 int16_t AP_Motors::apply_thrust_curve_and_volt_scaling(int16_t pwm_out, int16_t pwm_min, int16_t pwm_max) const
 {
-    float temp_out = ((float)(pwm_out-pwm_min))/((float)(pwm_max-pwm_min));
-    if (_thrust_curve_expo > 0.0f){
-        temp_out = ((_thrust_curve_expo-1.0f) + safe_sqrt((1.0f-_thrust_curve_expo)*(1.0f-_thrust_curve_expo) + 4.0f*_thrust_curve_expo*_lift_max*temp_out))/(2.0f*_thrust_curve_expo*_batt_voltage_filt.get());
+    // convert to 0.0 to 1.0 ratio
+    float throttle_ratio = ((float)(pwm_out-pwm_min))/((float)(pwm_max-pwm_min));
+
+    // air density ratio is increasing in density / decreasing in altitude
+    // divide by air_density_ratio
+    if (_air_density_ratio > 0.3f) {
+        throttle_ratio /= constrain_float(_air_density_ratio,0.3f,1.25f);
     }
-    temp_out = constrain_float(temp_out*_thrust_curve_max*(pwm_max-pwm_min)+pwm_min, pwm_min, pwm_max);
-    return (int16_t)temp_out;
+
+    // constrain throttle_ratio within domain of throttle curve function
+    throttle_ratio = constrain_float(throttle_ratio,0.0f,1.0f);
+
+    // apply thrust curve - domain 0.0 to 1.0, range 0.0 to 1.0
+    if (_thrust_curve_expo > 0.0f){
+        throttle_ratio = ((_thrust_curve_expo-1.0f) + safe_sqrt((1.0f-_thrust_curve_expo)*(1.0f-_thrust_curve_expo) + 4.0f*_thrust_curve_expo*_lift_max*throttle_ratio))/(2.0f*_thrust_curve_expo*_batt_voltage_filt.get());
+    }
+
+    // scale to maximum thrust point
+    throttle_ratio *= _thrust_curve_max;
+
+    // convert back to pwm range, constrain and return
+    return (int16_t)constrain_float(throttle_ratio*(pwm_max-pwm_min)+pwm_min, pwm_min, (pwm_max-pwm_min)*_thrust_curve_max+pwm_min);
 }
 
 // update_lift_max from battery voltage - used for voltage compensation
