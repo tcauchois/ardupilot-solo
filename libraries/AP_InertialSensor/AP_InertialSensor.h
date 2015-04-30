4760763dc14b3303d67cc1a325c45604e4308f1f
Jonathan Challinger <mr.challinger@gmail.com>
2015-04-30 02:15:36 -0700

    AP_InertialSensor: work around gyro and accel errors on startup

--- libraries/AP_InertialSensor/AP_InertialSensor.h
+++ libraries/AP_InertialSensor/AP_InertialSensor.h
@@ -324,6 +324,10 @@ private:
     uint32_t _gyro_error_count[INS_MAX_INSTANCES];
 
     DataFlash_Class *_dataflash;
+
+    uint32_t _accel_startup_error_count[INS_MAX_INSTANCES];
+    uint32_t _gyro_startup_error_count[INS_MAX_INSTANCES];
+    bool _startup_error_counts_set;
 };
 
 #include "AP_InertialSensor_Backend.h"
